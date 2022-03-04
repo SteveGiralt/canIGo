15cbf836ebebab4e77cb8853f1da1093065edfae
Steve Giralt <steve.giralt@gmail.com>
2022-03-03 22:02:57 -0700

    Revert "Added controllers and refactor routers"
    This reverts commit 9a095b64943aefceab0b68ac35589567bbe0ca9f.

--- routes/bathrooms.js
+++ routes/bathrooms.js
@@ -4,29 +4,39 @@ const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, validateBathroom, isAuthor } = require("../middleware");
 const bathrooms = require("../controllers/bathrooms");
 
-router
-  .route("/")
-  .get(catchAsync(bathrooms.index))
-  .post(isLoggedIn, validateBathroom, catchAsync(bathrooms.createBathroom));
-
-router
-  .route("/:id")
-  .get(catchAsync(bathrooms.showBathroom))
-  .put(
-    isLoggedIn,
-    isAuthor,
-    validateBathroom,
-    catchAsync(bathrooms.editBathroom)
-  )
-  .delete(isLoggedIn, isAuthor, catchAsync(bathrooms.deleteBathroom));
+router.get("/", catchAsync(bathrooms.index));
 
 router.get("/new", isLoggedIn, bathrooms.renderNewForm);
+
+router.post(
+  "/",
+  isLoggedIn,
+  validateBathroom,
+  catchAsync(bathrooms.createBathroom)
+);
+
+router.get("/:id", catchAsync(bathrooms.showBathroom));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(bathrooms.renderEditForm)
+);
+
+router.put(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  validateBathroom,
+  catchAsync(bathrooms.editBathroom)
+);
+
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(bathrooms.deleteBathroom)
 );
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,17 +2,39 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 const catchAsync = require("../utils/catchAsync");
+
+const Bathroom = require("../models/bathroom");
+const Review = require("../models/review");
+
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
-const { createReview, deleteReview } = require("../controllers/reviews");
-
-router.post("/", isLoggedIn, validateReview, catchAsync(createReview));
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  catchAsync(async (req, res) => {
+    const bathroom = await Bathroom.findById(req.params.id);
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
+    bathroom.reviews.push(review);
+    await review.save();
+    await bathroom.save();
+    req.flash("success", "Thanks for your feedback!");
+    res.redirect(`/bathrooms/${bathroom._id}`);
+  })
+);
 
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(deleteReview)
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Bathroom.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review deleted :(");
+    res.redirect(`/bathrooms/${id}`);
+  })
 );
 
 module.exports = router;
