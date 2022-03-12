4cc5f73a7c978031cc36e3ddeee5ab3206eaccdb
Steve Giralt <steve.giralt@gmail.com>
2022-03-11 19:25:34 -0700

    deployment tweaks

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const userRoutes = require("./routes/users");
 
 const databaseURL = process.env.DB_URL || "mongodb://localhost:27017/canigo";
 
+//
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
   "https://api.tiles.mapbox.com/",
@@ -79,7 +80,7 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: true,
+    secure: false,
     httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
